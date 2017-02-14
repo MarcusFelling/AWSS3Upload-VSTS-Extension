9fc5930f370da944abd9bb99cd5198e7684e321a
Marcus Felling <MarcusFelling@users.noreply.github.com>
2017-02-14 15:50:38 -0600

    1.3.0

--- AWSS3Upload/S3UploadTask.ps1
+++ AWSS3Upload/S3UploadTask.ps1
@@ -1,27 +1,47 @@
 [CmdletBinding()]
 param()
 
+Function FileUpload($Files, $Key) {    
+        # Begin Upload
+        Write-Output "Uploading $Files"
+        Write-S3Object -BucketName $BucketName -File $Files -Key $Key 
+        Write-Output "Upload complete!"
+}
+
+Function FolderUpload($FileList) {
+               
+    Write-Output "Uploading all files in $Files"
+    Get-ChildItem -recurse $Files |
+    ForEach {
+        # Begin Upload
+        Write-S3Object -BucketName $BucketName -File $_.FullName -Key $_.Name
+        Write-Output "$_ Upload complete!"
+    }        
+}
+
 Trace-VstsEnteringInvocation $MyInvocation
-
 Try {
-
-    # Get values defined in task.json
-    $BucketName = Get-VstsInput -Name BucketName -Require
-    $File = Get-VstsInput -Name File -Require
-    $Key = Get-VstsInput -Name Key -Require
     
     # Load AWS Tools for PowerShell to upload to S3
-    If (-not (Get-Module -Name "AWSPowerShell")) 
-    {
-        write-host "Loading AWSPowerShell module"
+    If (-not (Get-Module -Name "AWSPowerShell")) {
+        Write-Output "Loading AWSPowerShell module"
         Import-Module AWSPowerShell
-    }   
+    }            
 	
-    # Begin Upload
-    Write-Output "Upload $File to $BucketName$Key"
-    Write-Output "Running command: Write-S3Object -BucketName $BucketName -File $File -Key $Key"  
-    Write-S3Object -BucketName $BucketName -File $File -Key $Key
-    Write-Output "Upload complete!"
+    # Get values defined in task.json
+    $BucketName = Get-VstsInput -Name BucketName -Require
+    $Files = Get-VstsInput -Name File -Require
+    $Key = Get-VstsInput -Name Key
+    
+    # If keylist isn't null, run file upload
+    If($Key){
+        write-output "File Upload"
+        FileUpload $Files $Key       
+    }
+    Else{
+        write-output "Folder Upload"
+        FolderUpload $Files
+    }   
 }
 
 Catch{
