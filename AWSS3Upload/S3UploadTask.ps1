bc968bfb0347c20c5acb1b9a2dc2624b1302ec3c
Marcus Felling <MarcusFelling@users.noreply.github.com>
2017-02-15 08:22:57 -0600

    1.3.1

--- AWSS3Upload/S3UploadTask.ps1
+++ AWSS3Upload/S3UploadTask.ps1
@@ -31,17 +31,21 @@ Try {
     # Get values defined in task.json
     $BucketName = Get-VstsInput -Name BucketName -Require
     $Files = Get-VstsInput -Name File -Require
-    $Key = Get-VstsInput -Name Key
-    
-    # If keylist isn't null, run file upload
-    If($Key){
-        write-output "File Upload"
+    $Key = Get-VstsInput -Name Key    
+
+    # If $Files is NOT a folder, run FileUpload
+    If((Get-Item $Files) -isnot [System.IO.DirectoryInfo]){
+        write-output "Running file Upload"
         FileUpload $Files $Key       
     }
-    Else{
-        write-output "Folder Upload"
-        FolderUpload $Files
-    }   
+    # If $Files is a folder, run FolderUpload
+    ElseIf((Get-Item $Files) -is [System.IO.DirectoryInfo]){
+        write-output "Running folder Upload"
+        FolderUpload $Files     
+    }
+    Else{        
+        Throw "Specified file or folder does not exist."
+    }
 }
 
 Catch{
